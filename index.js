eaba51420b0c4040c65b96457375cfb16a98a04e
Izzudin Fasya <izzudinfasya@gmail.com>
2025-04-25 02:03:05 +0800

    test new update serverless cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'http://checkouttest.financialdigitalmarketingevents.com',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello from Express on Vercel!');
