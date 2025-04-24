a7c057c2e6374965c7d9f76cb93067c1fc9571b2
Izzudin Fasya <izzudinfasya@gmail.com>
2025-04-25 02:18:03 +0800

    test new update serverless cors

--- api/index.js
+++ api/index.js
@@ -2,26 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// CORS middleware
 app.use(cors());
-
-// Middleware to handle JSON
 app.use(express.json());
 
-// Defining routes
 app.get('/', (req, res) => {
     res.send('Hello from Express on Vercel!');
 });
 
-// Fallback route for API
 app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello from API!' });
 });
 
-// You can add other routes as needed, for example:
-// app.get('/api/events', (req, res) => { ... });
-
 module.exports = (req, res) => {
-    // Handling the request and response using Express
-    app(req, res); // Passing request and response to Express
+    app(req, res);
 };
