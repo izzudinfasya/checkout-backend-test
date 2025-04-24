b08593a7bb2864fa0eb3d78edbe454310f4946a5
Izzudin Fasya <izzudinfasya@gmail.com>
2025-04-25 01:21:03 +0800

    test new update 3

--- api/events.js
+++ api/events.js
@@ -22,7 +22,7 @@ const getToken = async () => {
 
 const getEvents = async (token) => {
     const response = await axios.get(
-        'https://api.swoogo.com/api/v1/events?fields=capacity,close_date,...', // Your fields here
+        'https://api.swoogo.com/api/v1/events?fields=capacity,close_date,close_time,created_at,created_by,description,end_date,end_time,folder_id,free_event,hashtag,id,name,organizer_id,start_date,start_time,status,target_attendance,timezone,type_id,updated_at,updated_by,url,webinar_url',
         {
             headers: {
                 'Authorization': `Bearer ${token}`,
