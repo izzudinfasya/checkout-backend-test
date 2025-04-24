00e4d6bcedd605fd4411d7b5bcc6bef1aaba9f6a
Izzudin Fasya <izzudinfasya@gmail.com>
2025-04-25 02:20:00 +0800

    test new update serverless cors

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
 
-app.use(cors());
-app.use(express.json());
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express on Vercel!');
-});
-
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from API!' });
-});
-
-module.exports = (req, res) => {
-    app(req, res);
-};
+module.exports = allowCors(handler)
